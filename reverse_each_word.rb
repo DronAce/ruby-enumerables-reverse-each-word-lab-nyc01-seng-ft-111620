ca1ec5f40242780e527e57a566a5d2e012e5c58e
Okechukwu Uneze <unezeo@gmail.com>
2021-03-23 20:18:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,12 @@
 def reverse_each_word(string)
   s = string.split(" ")
+<<<<<<< HEAD
   s.collect do |x|
     x.reverse!
+=======
+  s.each do |x|
+    x = x.reverse
+>>>>>>> fa7111715674a3c78e8b0a1e798457d4d3b30eff
   end
   s.join(' ')
 end
